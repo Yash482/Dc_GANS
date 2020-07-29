1423e700a8c02f7f6d9c77cf6d1b077f1fea7f1c
Yash Bansal <agarwaly482@gmail.com>
2020-07-29 23:01:35 +0530

    f Please enter the commit message for your changes. Lines starting

--- dcgan.py
+++ dcgan.py
@@ -8,112 +8,247 @@ import torchvision.utils as vutils
 from torch.autograd import Variable
 import numpy as np
 import matplotlib as plt
+import pickle as pkl
+import torch.nn.functional as F
 
-#get dateset
-#create dataloader
-def get_dataloader(batch_size, image_size, data_dir = 'train/'):
-    transform = transforms.Compose([transforms.Resize(image_size), transforms.CenterCrop(image_size), transforms.ToTensor()])
-    dataset = dset.ImageFolder(data_dir, transform = transform )
-    dataloader = torch.utils.data.DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True)
+
+def get_dataloader(batch_size, image_size, data_dir='train/'):
+    """
+    Batch the neural network data using DataLoader
+    :param batch_size: The size of each batch; the number of images in a batch
+    :param img_size: The square size of the image data (x, y)
+    :param data_dir: Directory where image data is located
+    :return: DataLoader with batched data
+    """
+    transform = transforms.Compose([transforms.Resize(image_size),transforms.CenterCrop(image_size),transforms.ToTensor()])
+  
+    dataset = dset.ImageFolder(data_dir,transform = transform)
+    
+    dataloader = torch.utils.data.DataLoader(dataset = dataset,batch_size = batch_size,shuffle = True)
     return dataloader
+# Define function hyperparameters
+batch_size = 256
+img_size = 32
+# Call your function and get a dataloader
+celeba_train_loader = get_dataloader(batch_size, img_size)
 
-# Setting some hyperparameters
-batch_size = 256 # We set the size of the batch.
-image_size = 32 # We set the size of the generated images (64x64).
-
-train_loader = get_dataloader(batch_size, image_size)
-
-#scale the img pixel btw -1 to 1
 def scale(x, feature_range=(-1, 1)):
-    #this function assumes that the img is already scaled from 0-1
+    ''' Scale takes in an image x and returns that image, scaled
+       with a feature_range of pixel values from -1 to 1. 
+       This function assumes that the input x is already scaled from 0-1.'''
+    # assume x is scaled to (0, 1)
+    # scale to feature_range and return scaled x
     min, max = feature_range
-    x = x*(max- min) + min
+    x = x*(max-min) + min
     return x
 
-# Defining the weights_init function that takes as input a neural network m and that will initialize all its weights.
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        m.weight.data.normal_(0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        m.weight.data.normal_(1.0, 0.02)
-        m.bias.data.fill_(0)
-
-
-#create Discriminator
+def conv(input_c,output,kernel_size,stride = 2,padding  = 1, batch_norm = True):
+    layers =[]
+    con = nn.Conv2d(input_c,output,kernel_size,stride,padding,bias = False)
+    layers.append(con)
+    
+    if batch_norm:
+        layers.append(nn.BatchNorm2d(output))
+    
+    return nn.Sequential(*layers)
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self, conv_dim):
+        """
+        Initialize the Discriminator Module
+        :param conv_dim: The depth of the first convolutional layer
+        """
+        #complete init function
         super(Discriminator, self).__init__()
-        self.main = nn.Sequential( # We create a meta module of a neural network that will contain a sequence of modules (convolutions, full connections, etc.).
-            nn.Conv2d(3, 64, 4, 2, 1, bias = False), # We start with a convolution.
-            nn.LeakyReLU(0.2, inplace = True), # We apply a LeakyReLU.
-            nn.Conv2d(64, 128, 4, 2, 1, bias = False), # We add another convolution.
-            nn.BatchNorm2d(128), # We normalize all the features along the dimension of the batch.
-            nn.LeakyReLU(0.2, inplace = True), # We apply another LeakyReLU.
-            nn.Conv2d(128, 256, 4, 2, 1, bias = False), # We add another convolution.
-            nn.BatchNorm2d(256), # We normalize again.
-            nn.LeakyReLU(0.2, inplace = True), # We apply another LeakyReLU.
-            nn.Conv2d(256, 512, 4, 2, 1, bias = False), # We add another convolution.
-            nn.BatchNorm2d(512), # We normalize again.
-            nn.LeakyReLU(0.2, inplace = True), # We apply another LeakyReLU.
-            nn.Conv2d(512, 1, 4, 1, 0, bias = False), # We add another convolution.
-            nn.Sigmoid() # We apply a Sigmoid rectification to break the linearity and stay between 0 and 1.
-        )
+        self.conv_dim = conv_dim
+        self.layer_1 = conv(3,conv_dim,4,batch_norm = False) #16
+        self.layer_2 = conv(conv_dim,conv_dim*2,4) #8
+        self.layer_3 = conv(conv_dim*2,conv_dim*4,4) #4
+        self.fc = nn.Linear(conv_dim*4*4*4,1)
+    def forward(self, x):
+        """
+        Forward propagation of the neural network
+        :param x: The input to the neural network     
+        :return: Discriminator logits; the output of the neural network
+        """
+        # define feedforward behavior
+        x = F.leaky_relu(self.layer_1(x))
+        x = F.leaky_relu(self.layer_2(x))
+        x = F.leaky_relu(self.layer_3(x))
+        x = x.view(-1,self.conv_dim*4*4*4)
+        x = self.fc(x)
+        return x
 
-    def forward(self, input): # We define the forward function that takes as argument an input that will be fed to the neural network, and that will return the output which will be a value between 0 and 1.
-        output = self.main(input) # We forward propagate the signal through the whole neural network of the discriminator defined by self.main.
-        return output.view(-1) # We return the output which will be a value between 0 and 1.
+def deconv(input_c,output,kernel_size,stride = 2, padding =1, batch_norm = True):
+    layers = []
+    decon = nn.ConvTranspose2d(input_c,output,kernel_size,stride,padding,bias = False)
+    layers.append(decon)
+    
+    if batch_norm:
+        layers.append(nn.BatchNorm2d(output))
+    return nn.Sequential(*layers)
+class Generator(nn.Module):
+    
+    def __init__(self, z_size, conv_dim):
+        """
+        Initialize the Generator Module
+        :param z_size: The length of the input latent vector, z
+        :param conv_dim: The depth of the inputs to the *last* transpose convolutional layer
+        """
+        super(Generator, self).__init__()
+        # complete init function
+        self.conv_dim = conv_dim
+        self.fc = nn.Linear(z_size,conv_dim*8*2*2)
+        self.layer_1 = deconv(conv_dim*8,conv_dim*4,4) #4
+        self.layer_2 = deconv(conv_dim*4,conv_dim*2,4) #8
+        self.layer_3 = deconv(conv_dim*2,conv_dim,4) #16
+        self.layer_4 = deconv(conv_dim,3,4,batch_norm = False) #32
+        
+        
+    def forward(self, x):
+        """
+        Forward propagation of the neural network
+        :param x: The input to the neural network     
+        :return: A 32x32x3 Tensor image as output
+        """
+        # define feedforward behavior
+        x = self.fc(x)
+        x = x.view(-1,self.conv_dim*8,2,2) #(batch_size,depth,width,height)
+        x = F.relu(self.layer_1(x))
+        x = F.relu(self.layer_2(x))
+        x = F.relu(self.layer_3(x))
+        x = torch.tanh(self.layer_4(x))
+        return x
+    
+def weights_init_normal(m):
+    """
+    Applies initial weights to certain layers in a model .
+    The weights are taken from a normal distribution 
+    with mean = 0, std dev = 0.02.
+    :param m: A module or layer in a network    
+    """
+    # classname will be something like:
+    # `Conv`, `BatchNorm2d`, `Linear`, etc.
+    classname = m.__class__.__name__
+    
+    if hasattr(m,'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+        
+        m.weight.data.normal_(0.0,0.02)
+    
+        if hasattr(m,'bias') and m.bias is not None:
+            m.bias.data.zero_()
+            
+def build_network(d_conv_dim, g_conv_dim, z_size):
+    # define discriminator and generator
+    D = Discriminator(d_conv_dim)
+    G = Generator(z_size=z_size, conv_dim=g_conv_dim)
+# initialize model weights
+    D.apply(weights_init_normal)
+    G.apply(weights_init_normal)
+    print(D)
+    print()
+    print(G)
+    
+    return D, G
+   
+# Define model hyperparams
+d_conv_dim = 64
+g_conv_dim = 64
+z_size = 100
+D, G = build_network(d_conv_dim, g_conv_dim, z_size)
 
+def real_loss(D_out):
+    '''Calculates how close discriminator outputs are to being real.
+       param, D_out: discriminator logits
+       return: real loss'''
+    batch_size = D_out.size(0)
+    labels = torch.ones(batch_size)
+    criterion = nn.BCEWithLogitsLoss()
+    loss = criterion(D_out.squeeze(),labels)
+    return loss
+def fake_loss(D_out):
+    '''Calculates how close discriminator outputs are to being fake.
+       param, D_out: discriminator logits
+       return: fake loss'''
+    batch_size = D_out.size(0)
+    labels = torch.zeros(batch_size)
+    criterion =  nn.BCEWithLogitsLoss()
+    loss = criterion(D_out.squeeze(),labels)
+    return loss
 
-class Generator(nn.Module): # We introduce a class to define the generator.
+# Create optimizers for the discriminator D and generator G
+d_optimizer = optim.Adam(D.parameters(),lr = .0002, betas = [0.5,0.999])
+g_optimizer = optim.Adam(G.parameters(),lr = .0002, betas = [0.5,0.999])
 
-    def __init__(self): # We introduce the __init__() function that will define the architecture of the generator.
-        super(Generator, self).__init__() # We inherit from the nn.Module tools.
-        self.main = nn.Sequential( # We create a meta module of a neural network that will contain a sequence of modules (convolutions, full connections, etc.).
-            nn.ConvTranspose2d(100, 512, 4, 1, 0, bias = False), # We start with an inversed convolution.
-            nn.BatchNorm2d(512), # We normalize all the features along the dimension of the batch.
-            nn.ReLU(True), # We apply a ReLU rectification to break the linearity.
-            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias = False), # We add another inversed convolution.
-            nn.BatchNorm2d(256), # We normalize again.
-            nn.ReLU(True), # We apply another ReLU.
-            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias = False), # We add another inversed convolution.
-            nn.BatchNorm2d(128), # We normalize again.
-            nn.ReLU(True), # We apply another ReLU.
-            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias = False), # We add another inversed convolution.
-            nn.BatchNorm2d(64), # We normalize again.
-            nn.ReLU(True), # We apply another ReLU.
-            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias = False), # We add another inversed convolution.
-            nn.Tanh() # We apply a Tanh rectification to break the linearity and stay between -1 and +1.
-        )
+def train(D, G, n_epochs, print_every=50):
+    '''Trains adversarial networks for some number of epochs
+       param, D: the discriminator network
+       param, G: the generator network
+       param, n_epochs: number of epochs to train for
+       param, print_every: when to print and record the models' losses
+       return: D and G losses'''
+    
+# keep track of loss and generated, "fake" samples
+    samples = []
+    losses = []
+# Get some fixed data for sampling. These are images that are held
+    # constant throughout training, and allow us to inspect the model's performance
+    sample_size=16
+    fixed_z = np.random.uniform(-1, 1, size=(sample_size, z_size))
+    fixed_z = torch.from_numpy(fixed_z).float()
+   
+# epoch training loop
+    for epoch in range(n_epochs):
+# batch training loop
+        for batch_i, (real_images, _) in enumerate(celeba_train_loader):
+            batch_size = real_images.size(0)
+            real_images = scale(real_images)
+          
+            # 1. Train the discriminator on real and fake ima.ges
+            d_optimizer.zero_grad()
+            d_out_real = D(real_images)
+            z = np.random.uniform(-1,1,size = (batch_size,z_size))
+            z = torch.from_numpy(z).float()
+            d_loss = real_loss(d_out_real) + fake_loss(D(G(z)))
+            d_loss.backward()
+            d_optimizer.step()
+            # 2. Train the generator with an adversarial loss
+            G.train()
+            g_optimizer.zero_grad()
+            z = np.random.uniform(-1,1,size = (batch_size,z_size))
+            z = torch.from_numpy(z).float()
+            g_loss = real_loss(D(G(z)))
+            g_loss.backward()
+            g_optimizer.step()
+            
+            # Print some loss stats
+            if batch_i % print_every == 0:
+                # append discriminator loss and generator loss
+                losses.append((d_loss.item(), g_loss.item()))
+                # print discriminator and generator loss
+                print('Epoch [{:5d}/{:5d}] | d_loss: {:6.4f} | g_loss: {:6.4f}'.format(
+                        epoch+1, n_epochs, d_loss.item(), g_loss.item()))
+            
+            if batch_i % 50 == 0: # Every 100 steps:
+                vutils.save_image(real_images, '%s/real_samples.png' % "./results", normalize = True) # We save the real images of the minibatch.
+                fake = G(z) # We get our fake generated images.
+                vutils.save_image(fake.data, '%s/fake_samples_epoch_%03d.png' % ("./results", epoch), normalize = True) # We also save the fake generated images of the minibatch.
 
-    def forward(self, input): # We define the forward function that takes as argument an input that will be fed to the neural network, and that will return the output containing the generated images.
-        output = self.main(input) # We forward propagate the signal through the whole neural network of the generator defined by self.main.
-        return output # We return the output containing the generated images.
-
-G = Generator()
-G.apply(weights_init)
-
-D = Discriminator()
-D.apply(weights_init)
-
-print(D)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+## AFTER EACH EPOCH##    
+        # this code assumes your generator is named G, feel free to change the name
+        # generate and save sample, fake images
+        G.eval() # for generating samples
+        samples_z = G(fixed_z)
+        samples.append(samples_z)
+        G.train() # back to training mode
+# Save training generator samples
+    with open('train_samples.pkl', 'wb') as f:
+        pkl.dump(samples, f)
+    
+    # finally return losses
+    return losses
+    
+    
+# set number of epochs 
+n_epochs = 5
+# call training function
+losses = train(D, G, n_epochs=n_epochs)
